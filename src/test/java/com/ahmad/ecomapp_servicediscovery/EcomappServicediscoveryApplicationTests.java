ad8df931dd216f13e21fc42c8b20ba102f3871fd
Ahmad Rashidh <41289756+ahmadrashidh@users.noreply.github.com>
2025-02-12 15:56:27 +0000

    eureka server configured

---
+++ src/test/java/com/ahmad/ecomapp_servicediscovery/EcomappServicediscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmad.ecomapp_servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomappServicediscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
